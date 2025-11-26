4c66d3139764acda50d4206c01476c441d8ef443
Harshini Mangaleshkar <harshinimangaleshkar@utexas.edu>
2025-11-26 11:37:42 -0600

    Update schema.sql

--- supabase/schema.sql
+++ supabase/schema.sql
@@ -1,1 +1,10 @@
-select now();
+create table books (
+    id uuid primary key default uuid_generate_v4(),
+    user_id uuid references auth.users(id),
+    title text,
+    audio_url text,
+    created_at timestamp default now()
+);
+
+insert into storage.buckets (id, name)
+values ('audiobooks','audiobooks');
