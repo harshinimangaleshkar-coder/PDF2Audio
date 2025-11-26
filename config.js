e2f3b22c649c30c9a790dd92569e33b8e129619e
Harshini Mangaleshkar <harshinimangaleshkar@utexas.edu>
2025-11-26 13:11:24 -0600

    Update config.js

--- config.js
+++ config.js
@@ -4,4 +4,4 @@ export const SUPABASE_URL = "https://uaarjksdwixeuzqvdmss.supabase.co";
 export const SUPABASE_ANON_KEY =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVhYXJqa3Nkd2l4ZXV6cXZkbXNzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQxNjYwNzAsImV4cCI6MjA3OTc0MjA3MH0.VKBYWE9lB40VC6ejnapY94STVIBstdrt5cUOP4Brkr4";
 
-export const OPENAI_API_KEY = "YOUR_OPENAI_API_KEY";
+
